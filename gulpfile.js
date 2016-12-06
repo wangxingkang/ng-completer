07a3d78dab2778d43585331803d42c3fd14f1850
wangxingkang <156148958@qq.com>
2016-12-06 12:35:56 +0800

    update gulp  task build

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,23 @@ var gulp = require('gulp');
 var karma = require('gulp-karma');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
 
-gulp.task('build', function(done) {
+gulp.task('build', ['js', 'css'], function() {
+
+});
+
+gulp.task('js', function () {
   return gulp.src('src/ngCompleter.js')
     .pipe(uglify())
     .pipe(rename('ngCompleter.min.js'))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function () {
+  return gulp.src('src/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('test', function () {
